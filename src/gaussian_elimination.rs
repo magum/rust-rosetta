47ca8d162aa67536e392d4b38adc3457c7bbad6e
magum <magum1977@gmail.com>
2015-07-27 03:24:40 +0300

    some refactor and heap allocation free

--- src/gaussian_elimination.rs
+++ src/gaussian_elimination.rs
@@ -14,30 +14,28 @@ impl<'a, T:'a> Matrix<'a, T> where T: 'a + Copy {
         Matrix{m : _m, n : _n}
     }
 
-    fn val(&self, idx : Idx) -> & T  {
-        & (*self.m)[idx.0 + idx.1 * self.n]
-    }
-
-    fn val_mut<'b>(&'b mut self, idx : Idx) -> &'b mut T {
+    fn val<'b>(&'b mut self, idx : Idx) -> &'b mut T {
         & mut (*self.m)[idx.0 + idx.1 * self.n]
-    }
-
-    fn get_row_slice(&self, row: usize) -> &[T]  {
-        & self.m[row * self.n .. row * self.n + self.n ]
     }
 }
 
 
+
+fn get_row_slice<T>(m : &[T], n : usize, row: usize) -> &[T]  {
+    & m[ row * n .. row * n + n ]
+}
+
 struct MatrixRowIter<'a, T:'a> where T: Copy {
-    m : &'a Matrix<'a, T>,
+    m : &'a [T],
+    n : usize,
     row : usize,
     col : usize,
     used: &'a [bool]
 }
 
 impl<'a, T> MatrixRowIter<'a, T> where T: Copy {
-    fn new (_m : &'a Matrix<'a, T>, _used: &'a mut [bool], _col : usize) -> MatrixRowIter<'a, T> {
-        MatrixRowIter{m : _m, row : 0, col : _col, used : _used }
+    fn new (_m : &'a [T], _n: usize, _used: &'a mut [bool], _col : usize) -> MatrixRowIter<'a, T> {
+        MatrixRowIter{m : _m, n: _n, row : 0, col : _col, used : _used }
     }
 }
 
@@ -47,7 +45,7 @@ impl<'a, T> Iterator for MatrixRowIter<'a, T> where T: Copy {
     type Item = (T, usize);
     fn next(&mut self) -> Option<(T, usize)> {
         loop {
-            if self.row >= self.m.n {
+            if self.row >= self.n {
                 return None
             }
 
@@ -58,10 +56,10 @@ impl<'a, T> Iterator for MatrixRowIter<'a, T> where T: Copy {
             self.row += 1;
         }
 
-        let v = self.m.val(Idx(self.col, self.row));
+        let v = self.m[self.col + self.row * self.n];
         let ret = self.row;
         self.row += 1;
-        Some((*v, ret))
+        Some((v, ret))
     }
 }
 
@@ -69,32 +67,36 @@ fn main() {
 
     const MAT_SIZE : usize = 6;
 
-    let mut v : Vec<f64> = vec![
-    1.00, 0.00, 0.00,  0.00,  0.00, 0.00,
-    1.00, 0.63, 0.39,  0.25,  0.16, 0.10,
-    1.00, 1.26, 1.58,  1.98,  2.49, 3.13,
-    1.00, 1.88, 3.55,  6.70, 12.62, 23.80,
-    1.00, 2.51, 6.32, 15.88, 39.90, 100.28,
-    1.00, 3.14, 9.87, 31.01, 97.41, 306.02  ];
+    let v : Vec<f64> = vec![
+	    1.00, 0.00, 0.00,  0.00,  0.00, 0.00,
+	    1.00, 0.63, 0.39,  0.25,  0.16, 0.10,
+	    1.00, 1.26, 1.58,  1.98,  2.49, 3.13,
+	    1.00, 1.88, 3.55,  6.70, 12.62, 23.80,
+	    1.00, 2.51, 6.32, 15.88, 39.90, 100.28,
+	    1.00, 3.14, 9.87, 31.01, 97.41, 306.02  ];
 
     let b: [f64; MAT_SIZE] = [-0.01, 0.61, 0.91, 0.99, 0.60, 0.02];
 
-    let mat = Matrix::new(&mut v, MAT_SIZE);
-
-    let mut r : Vec<f64> = Vec::new();
     let mut used: [bool; MAT_SIZE] = [false; MAT_SIZE];
 
     let mut bm: [f64; MAT_SIZE] = unsafe{std::mem::uninitialized()};
 
+    let mut r: [f64; MAT_SIZE*MAT_SIZE] = unsafe{std::mem::uninitialized()};
+
     for i in 0..MAT_SIZE {
         let max = {
-            let mat_iter = MatrixRowIter::new(&mat, &mut used, i);
+            let mat_iter = MatrixRowIter::new(&v, MAT_SIZE, &mut used, i);
             mat_iter.fold((0.0, -1), |max, item| if item.0.abs() >= max.0 { item } else { max } )
         };
 
         used[max.1] = true;
 
-        r.extend(mat.get_row_slice(max.1).iter().cloned());
+		    for (i, o) in get_row_slice(&v, MAT_SIZE, max.1).iter()
+		    																								.zip(r[ i * MAT_SIZE .. (i+1) * MAT_SIZE ]
+		    																										.iter_mut()) {
+		        *o = i.clone();
+		    }
+
         bm[i] = b[max.1];
     }
 
@@ -104,9 +106,9 @@ fn main() {
         for row in dia+1..mat_out.n {
             let tmp = *mat_out.val(Idx(dia, row)) / *mat_out.val(Idx(dia, dia));
             for col in dia+1..mat_out.n {
-                *mat_out.val_mut(Idx(col, row)) -= tmp * *mat_out.val(Idx(col, dia));
+                *mat_out.val(Idx(col, row)) -= tmp * *mat_out.val(Idx(col, dia));
             }
-            *mat_out.val_mut(Idx(dia, row)) = 0.0;
+            *mat_out.val(Idx(dia, row)) = 0.0;
             bm[row] -= tmp * bm[dia];
         }
     }
